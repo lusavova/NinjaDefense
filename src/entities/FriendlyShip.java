a19925a7240cc7aa8baff85c595339d915e32fcc
Smytt <parushev.kiril@gmail.com>
2018-05-02 14:22:54 +0300

    edits

--- src/entities/FriendlyShip.java
+++ src/entities/FriendlyShip.java
@@ -14,7 +14,6 @@ public class FriendlyShip extends Ship {
         this.setSize(new Size(60, 99));
         this.setPosition(new Position((Constants.WIDTH - getSize().getWidth()) / 2, Constants.HEIGHT - 150));
         this.setImage(game.getSpriteSheet().getImage(0, 0, getSize().getWidth(), getSize().getHeight()));
-        this.addUpdateable();
     }
 
     @Override
