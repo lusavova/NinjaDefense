9ac949b5b29ea13eecac9e997dbf5c0216b50b58
Luchia Savova <lu.savova1@gmail.com>
2018-05-04 12:49:10 +0300

    change constuctors

--- src/telerik/entities/flying_objects/Doughnut.java
+++ src/telerik/entities/flying_objects/Doughnut.java
@@ -8,12 +8,12 @@ import telerik.interfaces.Entity;
 
 public class Doughnut extends Entity implements Collectable {
 
-    public Doughnut(PlayState game) {
+    public Doughnut(PlayState game, int x, int y) {
         super(game);
-        this.setSize(new Size(25, 23));
-        this.setPosition(new Position(20, 20));
-        this.setImage(game.getSpriteSheet().getImage(500, 0, getSize().getWidth(), getSize().getHeight()));
 
+        this.setSize(new Size(25, 23));
+        this.setPosition(new Position(x, y));
+        this.setImage(game.getSpriteSheet().getImage(500, 0, getSize().getWidth(), getSize().getHeight()));
     }
 
 
