5ebad64095cc0f5d45fa11dd7679d9fdb5b52c80
Luchia Savova <lu.savova1@gmail.com>
2018-05-04 14:18:10 +0300

    Change: LinkedList to HashSet

--- src/telerik/Handler.java
+++ src/telerik/Handler.java
@@ -5,23 +5,22 @@ import telerik.interfaces.Entity;
 import telerik.interfaces.Movable;
 
 import java.awt.*;
-import java.util.ArrayList;
 import java.util.HashSet;
 import java.util.LinkedList;
 
 public class Handler {
     private PlayState game;
-    private LinkedList<Entity> gameObjects;
-    private LinkedList<Movable> movables;
-    private LinkedList<Entity> gameObjectsTemp;
-    private LinkedList<Movable> movablesTemp;
+    private HashSet<Entity> gameObjects;
+    private HashSet<Movable> movables;
+    private HashSet<Entity> gameObjectsTemp;
+    private HashSet<Movable> movablesTemp;
 
     public Handler(PlayState game) {
         this.game = game;
-        this.gameObjects = new LinkedList<>();
-        this.movables = new LinkedList<>();
-        this.gameObjectsTemp = new LinkedList<>();
-        this.movablesTemp = new LinkedList<>();
+        this.gameObjects = new HashSet<>();
+        this.movables = new HashSet<>();
+        this.gameObjectsTemp = new HashSet<>();
+        this.movablesTemp = new HashSet<>();
     }
 
     public void render(Graphics2D g) {
