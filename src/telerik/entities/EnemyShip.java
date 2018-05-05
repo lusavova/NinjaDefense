11d12a21e0ba498f8fbaab1b4d0e65d879344eea
Smytt <parushev.kiril@gmail.com>
2018-05-05 23:28:55 +0300

    Enemy hit animation

--- src/telerik/entities/EnemyShip.java
+++ src/telerik/entities/EnemyShip.java
@@ -19,6 +19,7 @@ public class EnemyShip extends Ship implements CollidesWithOwnShip, CollidesWith
     private int shootDelay;
     private int width;
     private int height;
+    private int isHurting = 0;
 
     public EnemyShip(PlayState game, int level, int x, int y, int speed) {
         super(game);
@@ -57,6 +58,12 @@ public class EnemyShip extends Ship implements CollidesWithOwnShip, CollidesWith
 
     @Override
     public void update() {
+        if (isHurting > 0) {
+            isHurting--;
+            if (isHurting == 0) {
+                restoreFrame();
+            }
+        }
         if (getHealth() <= 0) {
             getGame().getHandler().addToRemove(this);
         }
@@ -70,6 +77,27 @@ public class EnemyShip extends Ship implements CollidesWithOwnShip, CollidesWith
             speed *= -1;
         }
         getBounds().moveBounds(this);
+    }
+
+    private void restoreFrame() {
+        this.getImageList().clear();
+        if (getLevel() == 0) {
+            this.setImage(getGame().getSpriteSheet().getImage(0, 99, width, height));
+        }
+        else {
+            this.setImage(getGame().getSpriteSheet().getImage(0, 154, width, height));
+        }
+    }
+
+    private void updateFrame() {
+        isHurting = 4;
+        this.getImageList().clear();
+        if (getLevel() == 0) {
+            this.setImage(getGame().getSpriteSheet().getImage(100, 99, width, height));
+        }
+        else {
+            this.setImage(getGame().getSpriteSheet().getImage(68, 154, width, height));
+        }
     }
 
     @Override
@@ -98,6 +126,7 @@ public class EnemyShip extends Ship implements CollidesWithOwnShip, CollidesWith
 
     @Override
     public void onCollideWithBullet(FriendlyBullet bullet) {
+        updateFrame();
         setHealth(getHealth() - bullet.getPower());
         getGame().getPlayer().setPoints(getGame().getPlayer().getPoints() + bullet.getPower());
     }
